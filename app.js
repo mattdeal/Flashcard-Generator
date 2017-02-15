85c14da314886634c2d749c5f9a03d9b8d5210af
mwdeal1 <mwdeal1@gmail.com>
2017-02-15 18:38:09 -0500

    Add skeleton code

--- app.js
+++ app.js
@@ -35,5 +35,60 @@ function saveCards() {
 }
 
 //todo: determine way to gather card data
-// use inquirer to prompt for card type and then get fields
-// use commandline args card-type, arg1, arg2
+function mainMenu() {
+    //todo: show how many cards the user has created/loaded
+    //new cards, total cards
+
+    //todo: use inquirer to prompt the user for what they want to do
+    //todo: create a card - calls createCard();
+    //todo: save cards - appends all current cards to cards.txt as json object
+    //todo: load cards - loads all cards from cards.txt
+    //todo: view cards - shows a list of all cards and lets the user pick one to view
+    //todo: exit app - exit the application
+}
+
+// todo: prompt for which type of card to create, call appropriate function
+// todo: return to main menu when finished
+function createCard() {
+
+}
+
+// create a BasicFlashCard
+// todo: return to main menu when finished
+function createBasicFlashCard() {
+
+}
+
+// create a ClozeFlashCard
+// todo: return to main menu when finished
+function createClozeFlashCard() {
+
+}
+
+//todo: append all NEW cards to cards.txt
+// todo: return to main menu when finished
+function saveCards() {
+
+}
+
+//todo: load all cards from cards.txt
+// todo: return to main menu when finished
+function loadCards() {
+
+}
+
+//todo: show user a list of cards, when one is selected, call viewCard
+function viewCards() {
+
+}
+
+//todo: show the card front, prompt for input, then show answer
+//todo: return to main menu when finished
+function viewCard() {
+
+}
+
+//todo: exit the app
+function exit() {
+
+}
